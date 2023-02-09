e3ab95b653969d2e2ff3952d16e06351897075a5
rjgallac <rjgallac@hotmail.com>
2023-02-09 21:42:54 +0000

    service working

---
+++ README.md
@@ -0,0 +1,3 @@
+http://localhost:8080/ws/countries.wsdl
+
+curl --header "content-type: text/xml" -d @request.xml http://localhost:8080/ws > output.xmlcurl --header "content-type: text/xml" -d @request.xml http://localhost:8080/ws > output.xml
---
+++ soap-service/src/main/java/com/example/soapservice/endpoint/CountryEndpoint.java
@@ -0,0 +1,30 @@
+package com.example.soapservice.endpoint;
+
+import com.example.soapservice.models.GetCountryRequest;
+import com.example.soapservice.models.GetCountryResponse;
+import com.example.soapservice.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://soapservice.example.com/models";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
